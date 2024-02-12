fcdbed463b73f56535ad9bc8c14f7a2073f0dd07
shakib <codershakibs@gmail.com>
2024-02-12 18:59:48 +0600

    first commit

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,28 @@
+
+
+
+//
+$(document).ready(function () {
+    $('.popup-gallery').magnificPopup({
+        delegate: 'a',
+        type: 'image',
+        tLoading: 'Loading image #%curr%...',
+        mainClass: 'mfp-img-mobile',
+        gallery: {
+            enabled: true,
+            navigateByImgClick: true,
+            preload: [0, 2] // Will preload 0 - before current, and 1 after the current image
+        },
+        image: {
+            tError: '<a href="%url%">The image #%curr%</a> could not be loaded.',
+            titleSrc: function (item) {
+                return item.el.attr('title') + '<small>by Marsel Van Oosten</small>';
+            }
+        }
+    });
+});
+
+
 // ideal-card
 document.addEventListener("DOMContentLoaded", function () {
     let splideElements = document.querySelectorAll(".ideal-card-1");
@@ -135,6 +160,27 @@ document.addEventListener("DOMContentLoaded", function () {
         splide.mount();
     });
 });
+
+
+
+// When the user scrolls the page, execute myFunction
+window.onscroll = function () { myFunction() };
+
+// Get the header
+var header = document.getElementById("myHeader");
+
+// Get the offset position of the navbar
+var sticky = header.offsetTop;
+
+// Add the sticky class to the header when you reach its scroll position. Remove "sticky" when you leave the scroll position
+function myFunction() {
+    if (window.pageYOffset > sticky) {
+        header.classList.add("sticky");
+    } else {
+        header.classList.remove("sticky");
+    }
+}
+
 
 
 
